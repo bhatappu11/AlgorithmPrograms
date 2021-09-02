976cc85a3f6cf8ed4a223642005bdad2577e2eb0
Arpitha Bhat <arpithabht12@gmail.com>
2021-09-02 23:44:08 +0530

    REFACTOR: used generics to implement bubble sort

--- src/com/bridgelabz/algorithmprograms/BubbleSort.java
+++ src/com/bridgelabz/algorithmprograms/BubbleSort.java
@@ -1,12 +1,12 @@
 package com.bridgelabz.algorithmprograms;
 
-public class BubbleSort {
-	static void bubbleSort(int array[]) {
+public class BubbleSort<K extends Comparable<K>> {
+	public void bubbleSort(K[] array) {
 		int length = array.length;
 		for (int i = 0; i < length - 1; i++) {
 			for (int j = 0; j < length - i - 1; j++) {
-				if (array[j] > array[j + 1]) {
-					int temp = array[j];
+				if (array[i].compareTo(array[j + 1]) > 0) {
+					K temp = array[j];
 					array[j] = array[j + 1];
 					array[j + 1] = temp;
 				}
@@ -14,14 +14,15 @@ public class BubbleSort {
 		}
 		printArray(array);
 	}
-	static void printArray(int arr[]) {
+	public void printArray(K[] arr) {
 			int n = arr.length;
 			for (int i = 0; i < n; ++i)
 				System.out.print(arr[i] + " ");
 		}
 	public static void main(String[] args) {
-		int[] array = { 17, 36, 41, 25, 75, 5, 3 };
-		bubbleSort(array);
+		BubbleSort<Integer> sortObject= new BubbleSort<>();
+		Integer[] array = { 17, 36, 41, 25, 75, 5, 3 };
+		sortObject.bubbleSort(array);
 	}
 
 }
